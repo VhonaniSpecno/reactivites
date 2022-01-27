78c368d906863aa76dcbe6b2d5637538be6cfd51
Vhonani <vhonani@specno.com>
2022-01-27 14:05:25 +0200

    added error handling & validation

---
+++ react-activities/API/BuggyController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request");
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+        }
+
+        [HttpGet("unauthorised")]
+        public ActionResult GetUnauthorised()
+        {
+            return Unauthorized();
+        }
+    }
+}
--- react-activities/Application/Activities/Delete.cs
+++ react-activities/Application/Activities/Delete.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Persistence;
@@ -10,12 +11,12 @@ namespace Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command,Result<Unit>>
         {
         private readonly DataContext context;
             public Handler(DataContext context)
@@ -23,13 +24,15 @@ namespace Application.Activities
             this.context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await context.Activities.FindAsync(request.Id);
+                // if (activity == null) return null;
 
                 context.Remove(activity);
-                await context.SaveChangesAsync();
-                return Unit.Value;
+                var result = await context.SaveChangesAsync() >0;
+                if (!result) return Result<Unit>.Failure("Failed to delete the activity");
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
